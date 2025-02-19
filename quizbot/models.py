be92dfdc02c6c0b03b162b7e38dd73947fe3cc00
ouxrlo <ouxrlo3@gmail.com>
2025-02-19 22:45:14 +0900

    fix: model code change and migrate

--- quizbot/models.py
+++ quizbot/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator # 음수가 되지않도록
 
 # 로그인 없이 사용자 이름만 저장, 점수 관리
 class User(models.Model):
     username = models.CharField(max_length=30, unique=True) # 사용자 이름 중복 불가
-    score = models.IntegerField(default=0) # 시작 점수 0
+    score = models.IntegerField(default=0, validators=[MinValueValidator(0)]) # 시작 점수 0
 
     def __str__(self):
-        return f"{self.username} ({self.score}점"
+        return f"{self.username} ({self.score}점)"
 
 
 # 퀴즈 Category
@@ -19,9 +20,13 @@ class Category(models.Model):
 
 # 퀴즈 문제 저장
 class Question(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) # Category 연결
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True) # Category 연결
     text = models.TextField() # 문제 내용
     answer = models.CharField(max_length=255) # correct answer
+
+    def save(self, *args, **kwargs):
+        self.answer = self.answer.lower()  # 정답을 소문자로 변환 후 저장
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.text} - {self.answer}"
